f9bca3e58841249af06322bb8668596baf385164
quixotic <quixoticf@gmail.com>
2015-06-06 14:13:11 -0400

    Put map on page, add search nav

--- README.md
+++ README.md
@@ -20,3 +20,9 @@ A map of sentiment about specific topics by location.
     src="https://maps.googleapis.com/maps/api/js?libraries=visualization&sensor=true_or_false"></script>
   <script type="text/javascript"
     src="https://maps.googleapis.com/maps/api/js?<%= api_key %>"></script>
+
+
+    <form action="map#search" method="post">
+      I want to see how people feel about (thing) in (location)
+      <input type="submit">
+    </form>
---
+++ app/assets/javascripts/moodmapper.js
@@ -0,0 +1,53 @@
+(function() {
+  "use strict";
+  var citymap = {};
+  citymap['chicago'] = {
+    center: new google.maps.LatLng(41.878113, -87.629798),
+    tweetTotal: 2714856
+  };
+  citymap['newyork'] = {
+    center: new google.maps.LatLng(40.714352, -74.005973),
+    tweetTotal: 8405837
+  };
+  citymap['losangeles'] = {
+    center: new google.maps.LatLng(34.052234, -118.243684),
+    tweetTotal: 3857799
+  };
+  citymap['vancouver'] = {
+    center: new google.maps.LatLng(49.25, -123.1),
+    tweetTotal: 603502
+  };
+
+  var cityCircle;
+
+  function initialize() {
+    // Create the map.
+    var mapOptions = {
+      zoom: 12,
+      center: citymap['newyork'].center,//new google.maps.LatLng(37.09024, -95.712891),
+      mapTypeId: google.maps.MapTypeId.TERRAIN
+    };
+
+    var map = new google.maps.Map(document.getElementById('map-canvas'),
+        mapOptions);
+
+    // Construct the circle for each value in citymap.
+    // Note: We scale the area of the circle based on the tweetTotal.
+    for (var city in citymap) {
+      var populationOptions = {
+        strokeColor: '#FF0000',
+        strokeOpacity: 0.8,
+        strokeWeight: 2,
+        fillColor: '#FF0000',
+        fillOpacity: 0.7,
+        map: map,
+        center: citymap[city].center,
+        radius: Math.sqrt(citymap[city].tweetTotal)
+      };
+      // Add the circle for this city to the map.
+      cityCircle = new google.maps.Circle(populationOptions);
+    }
+  }
+
+  google.maps.event.addDomListener(window, 'load', initialize);
+}());
